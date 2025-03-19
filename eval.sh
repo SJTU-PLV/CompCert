6623a47d64b37762bad27eb4d750b3458fd4be31
Jinhua Wu <jinhua.wu@sjtu.edu.cn>
2025-03-19 21:59:04 +0800

    updated eval.sh

--- eval.sh
+++ eval.sh
@@ -3,12 +3,18 @@
 WC="coqwc"
 DIR="rustfrontend"
 PARSER="rustparser/Rustsurface.ml $DIR/Rustsyntax.v $DIR/Rustlightgen.v"
-RL="$DIR/Rusttypes.v $DIR/Rustlight.v $DIR/Rustlightown.v $DIR/InitDomain.v $DIR/RustOp.v"
+RL="$DIR/Rusttypes.v $DIR/Rustlight.v $DIR/Rustlightown.v $DIR/InitDomain.v $DIR/RustOp.v $DIR/Rusttyping.v"
 RIR="$DIR/RustIR.v $DIR/RustIRown.v $DIR/RustIRsem.v"
 IRgen="$DIR/RustIRgen.v $DIR/RustIRgenProof.v"
 ELAB="$DIR/RustIRcfg.v $DIR/InitAnalysis.v $DIR/ElaborateDrop.v $DIR/ElaborateDropProof.v"
 CLgen="$DIR/Clightgen.v $DIR/Clightgenspec.v $DIR/Clightgenproof.v"
+Owncheck="$DIR/MoveChecking.v $DIR/MoveCheckingFootprint.v $DIR/MoveCheckingDomain.v $DIR/MoveCheckingSafe.v"
+Safety="common/SmallstepLinkingSafe.v common/InvariantAlgebra.v common/SmallstepSafe.v"
+
 COMP="driver/RA.v driver/CallConvRust.v"
+
+echo "Open Safety, Compositionality and Preservation"
+$WC $Safety
 
 echo "Rustlight"
 $WC $RL
@@ -19,8 +25,11 @@ $WC $RIR
 echo "Parser"
 $WC $PARSER
 
-echo "RustIRgen"
+echo "Lowering"
 $WC $IRgen
+
+echo "Ownership Checking"
+$WC $Owncheck
 
 echo "Drop Elaboration"
 $WC $ELAB
@@ -31,5 +40,12 @@ $WC $CLgen
 echo "Simulation Convention"
 $WC $COMP
 
-echo "Total"
+HashMap="rustdemo/*.v"
+echo "Running Example"
+$WC $HashMap
+
+echo "Owl Compiler Total"
 $WC $RL $RIR $PARSER $IRgen $ELAB $CLgen $COMP
+
+echo "Total"
+$WC $Safety $RL $RIR $PARSER $IRgen $Owncheck $ELAB $CLgen $COMP
